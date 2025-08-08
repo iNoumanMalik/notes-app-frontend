5fc4f09062ec2071aaafbccb2cd9117d6ba5b3ef
Nouman Malik <inoumanmalik@outlook.com>
2025-08-08 12:47:51 +0500

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useState } from 'react';
+import { setToken } from './api';
+
+
+function App() {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/api.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+const api = axios.create({baseURL: 'http://localhost:8000/api'})
+
+export function setToken(token){
+    if(token) api.defaults.headers.common.Authorization = `Bearer ${token}`;
+    else delete api.defaults.headers.common.Authorization;
+}
+
+export default api;
